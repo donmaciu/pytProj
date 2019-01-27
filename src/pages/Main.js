9dd7ae5ba175f2d8811638747393435ccb0b0e87
donmaciu <donmaciu@interia.pl>
2019-01-27 15:50:22 +0100

    react

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import { ScaleLoader } from 'react-spinners';
 
 
 class Main extends Component {
@@ -7,12 +8,69 @@ class Main extends Component {
         super(props)
 
         this.state = {
-            value: ''
+            value: '',
+            scanner: false,
+            scannerTask: null,
         }
     }
 
     pushButton(btn) {
         this.setState({value: this.state.value + btn})
+    }
+
+    testScan(id) {
+        fetch('/api/readTask/' + id)
+        .then(res => {
+            if(res.status === 200) {
+                return res.json();
+            } else {
+                this.setState({scanner: false});
+
+                window.clearInterval(this.task)
+            }
+        })
+        .then(json => {
+            if(json) {
+                if(json.terminated && json.results) {
+                    this.parseResult(json.results)
+
+                    this.setState({scanner: false});
+                    window.clearInterval(this.task)
+                }
+            }
+
+            
+        })
+        .catch(err => {
+            this.setState({scanner: false});
+            window.clearInterval(this.task)
+        })
+    }
+
+    parseResult(list) {
+        if(list instanceof Array && list.length > 0) {
+            this.setState({value: list[0]})
+        }
+    }
+
+    startScanner() {
+        fetch('/api/readTask?timeout=5000')
+        .then(res => {
+            if(res.status === 200) {
+                return res.json();
+            }
+        })
+        .then(json => {
+            if(json) {
+                if(json.id) {
+                    this.task = window.setInterval(() => this.testScan(json.id), 500);
+                    this.setState({scanner: true})
+                }
+            }
+        })
+        .catch(err => {
+
+        })
     }
 
     render() {
@@ -46,7 +104,14 @@ class Main extends Component {
                     <div className='col-sm-3 p-2'><button className='btn btn-primary btn-block'>#</button></div>
                 </div>
                 <div className='row screen-footer'>
-                    <div className='col-sm-12 p-2'><button className='btn btn-success btn-block'>SKANUJ</button></div>
+                    <div className='col-sm-12 p-2'>
+                        <button className='btn btn-success btn-block' onClick={() => this.startScanner()}>
+                            { this.state.scanner 
+                                ? 
+                               <ScaleLoader color='white' />
+                                : 'SKANUJ' }
+                        </button>
+                    </div>
                 </div>
             </div>
             
